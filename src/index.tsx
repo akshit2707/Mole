5291facf07e5243affe76b4fa9b43a3381b492fe
AKSHIT KUMAR <akskumar3@publicisgroupe.net>
2021-11-01 16:33:32 +0530

    added files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React ,{Fragment,useState,Children} from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Moles = ({ children }) => <div>{children}</div>
+const Mole = () => <button>Mole</button>
+const Timer = () => <div>Time: 00:00</div>
+const Score = () => <div>Score: 0</div>
+
+const Game = () => (
+
+  const [playing, setPlaying] = useState<boolean>(false);
+  
+  return (
+    <Fragment>
+      {!playing && <h1>Whac-A-Mole</h1>}
+      <button onClick={() => setPlaying(!playing)}>
+        {playing ? "Stop" : "Start"}
+      </button>
+      {playing && (
+        <Fragment>
+          <Score />
+          <Timer />
+          <Moles>
+            <Mole />
+            <Mole />
+            <Mole />
+            <Mole />
+            <Mole />
+          </Moles>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+  
+
+
+ReactDOM.render(<Game/>, document.getElementById('root'))
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
